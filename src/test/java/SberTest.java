3238dd27f3a18bbb7b37a3845d70ca5a83704f73
Sonne <rikxela@gmail.com>
2019-09-04 19:21:15 +0300

    commit #2

--- src/test/java/SberTest.java
+++ src/test/java/SberTest.java
@@ -15,8 +15,8 @@ import static junit.framework.TestCase.assertTrue;
 public class SberTest {
     private WebDriver driver;
     private final String SOCIAL_TAG = "//li//a[contains(@class, 'social_link')]";
-    private final String REGION = "//a[contains(@class,'hd-ft-region')]";
-    private final String NREGION = "//input[contains(@class,'kit-input__control')][@type='search']";
+    //private final String REGION = "//a[contains(@class,'hd-ft-region')]";
+    private final String REGION = "//input[contains(@class,'kit-input__control')][@type='search']";
 // //span[@class=('footer__social_logo footer__social_fb') or (@class = 'footer__social_logo footer__social_tw') or
 // (@class = 'footer__social_logo footer__social_yt') or (@class = 'footer__social_logo footer__social_ins') or
 // (@class = 'footer__social_logo footer__social_vk') or (@class = 'footer__social_logo footer__social_ok') or
@@ -28,24 +28,16 @@ public class SberTest {
         String url = "http://www.sberbank.ru/ru/person";
         driver.manage().window().maximize();
         driver.get(url);
-        //Wait wait = new WebDriverWait(driver,5,1000);
-        driver.findElement(By.xpath(REGION)).click();
-
-        driver.findElement(By.xpath(NREGION)).sendKeys("Нижегородская область");
-
-        driver.findElement(By.xpath(NREGION)).click();
-
+        driver.findElement(By.className("hd-ft-region__title")).click();
+        driver.findElement(By.xpath(REGION)).sendKeys("Нижегородская область");
         driver.findElement(By.xpath("//a[contains(text(), 'Нижегородская область')]")).click();
-        System.out.println("wow");
         WebElement element = driver.findElement(By.xpath("//footer"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true)", element);
-
-        //}
     }
 
     @After
     public void asserts() throws Exception {
-        assertEquals("Нижегородская область", driver.findElement(By.xpath(REGION)).getText());
+        assertEquals("Нижегородская область", driver.findElement(By.className("hd-ft-region__title")).getText());
         assertTrue(driver.findElement(By.tagName("span")).isDisplayed());
         assertTrue(driver.findElement(By.tagName("span")).isEnabled());
         List<WebDriver> list = new ArrayList(driver.findElements(By.xpath(SOCIAL_TAG)));
